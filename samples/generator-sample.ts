b4686edacb119b20996184ef88dc2631f625d252
Mike Hopcroft <mhop@microsoft.com>
2019-02-28 16:57:09 -0800

    WIP: generator-sample.ts works

--- samples/generator-sample.ts
+++ samples/generator-sample.ts
@@ -1,38 +1,83 @@
-// import { Cart, Crew, HOUR, LocationId, Shift, SimTime, StaffingPlan, standardShift, adjustShift } from '../src';
+import { Cart, Crew, HOUR, Interval, LocationId, Shift, SimTime, StaffingPlan, standardShift, adjustShift, MIN_DATE, MAX_DATE } from '../src';
 
-// function go() {
-//     const home: LocationId = 0;
-//     const breakRoom: LocationId = 7;
-//     const day: Shift = standardShift(8 * HOUR, home, breakRoom);
-//     const swing: Shift = adjustShift(day, 8 * HOUR);
+function go() {
+    const home: LocationId = 0;
+    const breakRoom: LocationId = 7;
+    const day: Shift = standardShift(8 * HOUR, home, breakRoom);
+    const swing: Shift = adjustShift(day, 8 * HOUR);
+    const inService: Interval = {
+        start: day.working.start - 1,
+        end: swing.working.end + 1
+    };
 
-//     const crews: Crew[] = [
-//         {
-//             shift: day,
-//             size: 3
-//         },
-//         {
-//             shift: swing,
-//             size: 2
-//         }
-//     ];
+    PrintShift(day);
+    PrintShift(swing);
 
-//     const staff = new StaffingPlan(crews);
+    const crews: Crew[] = [
+        {
+            shift: day,
+            size: 3
+        },
+        {
+            shift: swing,
+            size: 2
+        }
+    ];
 
-//     console.log('Carts:');
-//     for (const cart of staff.carts()) {
-//         console.log(`  Cart ${cart.id} starts at ${cart.lastKnownLocation}`);
-//     }
+    const staff = new StaffingPlan(inService, crews);
 
-//     console.log('Jobs:');
-//     for (const job of staff.jobs()) {
-//         console.log(`  Job ${job.id}: cart ${(job.assignedTo as Cart).id} suspends at ${job.suspendLocation} between ${formatTime(job.suspendTime)} and ${formatTime(job.resumeTime)}.`);
-//     }
-// }
+    console.log('Carts:');
+    for (const cart of staff.carts()) {
+        console.log(`  Cart ${cart.id} starts at ${cart.lastKnownLocation}`);
+    }
 
-// function formatTime(time: SimTime) {
-//     const x = new Date(time);
-//     return x.toISOString().slice(11,16);
-// }
+    console.log('Jobs:');
+    const jobs = [...staff.jobs()].sort( (a, b) => {
+        if (a === b) {
+            return 0;
+        }
+        else {
+            const cartA = a.assignedTo as Cart;
+            const cartB = b.assignedTo as Cart;
+            if (cartA.id !== cartB.id) {
+                return cartA.id - cartB.id;
+            }
+            else {
+                return a.suspendTime - b.suspendTime;
+            }
+        }
+    });
 
-// go();
+    for (const job of jobs) {
+        console.log(`  Job ${job.id}: cart ${(job.assignedTo as Cart).id} suspends at ${job.suspendLocation} between ${formatTime(job.suspendTime)} and ${formatTime(job.resumeTime)}.`);
+    }
+}
+
+function formatTime(time: SimTime) {
+    const x = new Date(time);
+    // return x.toISOString();
+    if (x.valueOf() === MIN_DATE) {
+        return "MIN";
+    }
+    else if (x.valueOf() === MAX_DATE) {
+        return "MAX";
+    }
+    else {
+        const day = x.toISOString().slice(-16,-14);
+        const time = x.toISOString().slice(-13,-8);
+        return `${day} @ ${time}`;
+    }
+}
+
+function formatInterval(interval: Interval) {
+    return `${formatTime(interval.start)}-${formatTime(interval.end)}`;
+}
+
+function PrintShift(shift: Shift) {
+    console.log(`working: ${formatInterval(shift.working)}`);
+    for (const b of shift.breaks) {
+        console.log(`  break: ${formatInterval(b.interval)}`);
+    }
+}
+
+go();
--- src/generators/staffing-plan.ts
+++ src/generators/staffing-plan.ts
@@ -1,58 +1,55 @@
-export const foobar = 1;
+import { Clock, SimTime, start } from "../core";
+import { Cart, CartFactory, JobFactory, LocationId, OutOfServiceJob, Job } from "../environement";
 
-// import { Clock, SimTime, start } from "../core";
-// import { Cart, CartFactory, JobFactory, LocationId, OutOfServiceJob, Job } from "../environement";
-// import { JobMerger } from "../planner";
+// Mapping from common time units to the milliseconds used by the Date class.
+export const SECOND = 1000;
+export const MINUTE = SECOND * 60;
+export const HOUR = MINUTE * 60;
 
-// // Mapping from common time units to the milliseconds used by the Date class.
-// export const SECOND = 1000;
-// export const MINUTE = SECOND * 60;
-// export const HOUR = MINUTE * 60;
+export const MIN_DATE = -8640000000000000;
+export const MAX_DATE = 8640000000000000;
 
-// export const MIN_DATE = -8640000000000000;
-// export const MAX_DATE = 8640000000000000;
+// An interval of time.
+// The start time should never exceed the end ime.
+export interface Interval {
+    start: SimTime;
+    end: SimTime;
+}
 
-// // An interval of time.
-// // The start time should never exceed the end ime.
-// export interface Interval {
-//     start: SimTime;
-//     end: SimTime;
-// }
+// Description of an out-of-service period during a shift.
+// Breaks are to be taken at the specified location.
+export interface Break {
+    interval: Interval;
+    location: LocationId;
+}
 
-// // Description of an out-of-service period during a shift.
-// // Breaks are to be taken at the specified location.
-// export interface Break {
-//     interval: Interval;
-//     location: LocationId;
-// }
+// Represents a work shift.
+// Shift starts and ends at the home location.
+// Breaks have intervals and locations specified.
+export interface Shift {
+    working: Interval;
 
-// // Represents a work shift.
-// // Shift starts and ends at the home location.
-// // Breaks have intervals and locations specified.
-// export interface Shift {
-//     working: Interval;
+    // Breaks must obey the following conventions
+    //   1. Breaks do not overlap.
+    //   2. Breaks do not extend beyond working interval.
+    //   3. Breaks ordered by increasing start time.
+    breaks: Break[];
+    home: LocationId;
+}
 
-//     // Breaks must obey the following conventions
-//     //   1. Breaks do not overlap.
-//     //   2. Breaks do not extend beyond working interval.
-//     //   3. Breaks ordered by increasing start time.
-//     breaks: Break[];
-//     home: LocationId;
-// }
+// Represents a certain number of staff who all work the same shift.
+export interface Crew {
+    shift: Shift;
+    size: number;
+}
 
-// // Represents a certain number of staff who all work the same shift.
-// export interface Crew {
-//     shift: Shift;
-//     size: number;
-// }
-
-// function contains(outer: Interval, inner: Interval): boolean {
-//     return (
-//         outer.start < inner.start &&
-//         outer.start < inner.end &&
-//         outer.end > inner.start &&
-//         outer.end > inner.end);
-// }
+function contains(outer: Interval, inner: Interval): boolean {
+    return (
+        outer.start < inner.start &&
+        outer.start < inner.end &&
+        outer.end > inner.start &&
+        outer.end > inner.end);
+}
 
 // class CartSchedule {
 //     private readonly cartFactory: CartFactory;
@@ -94,184 +91,231 @@ export const foobar = 1;
 //     }
 // }
 
-// ///////////////////////////////////////////////////////////////////////////////
-// //
-// // StaffingPlan
-// //
-// // Creates a fleet of Carts and a set of OutOfServiceJobs corresponding to
-// // worker break times.
-// //
-// // DESIGN INTENT is to provide an easy means to configure a simulation based
-// // on synthetic events.
-// //
-// ///////////////////////////////////////////////////////////////////////////////
-// export class StaffingPlan {
-//     private readonly clock: Clock;
-//     private readonly cartFactory: CartFactory;
-//     private readonly jobFactory: JobFactory;
-//     private readonly pauses: OutOfServiceJob[];
-//     private readonly cartsByLocation: Map<LocationId, CartSchedule[]>;
-//     private readonly cartCapacity = 10;
+///////////////////////////////////////////////////////////////////////////////
+//
+// StaffingPlan
+//
+// Creates a fleet of Carts and a set of OutOfServiceJobs corresponding to
+// worker break times.
+//
+// DESIGN INTENT is to provide an easy means to configure a simulation based
+// on synthetic events.
+//
+///////////////////////////////////////////////////////////////////////////////
+export class StaffingPlan {
+    private readonly inService: Interval;
+    private readonly clock: Clock;
+    private readonly cartFactory: CartFactory;
+    private readonly jobFactory: JobFactory;
+    private readonly pauses: OutOfServiceJob[];
 
-//     constructor(crews: Crew[]) {
-//         this.clock = new Clock();
-//         this.cartFactory = new CartFactory();
-//         this.jobFactory = new JobFactory();
-//         this.pauses = [];
-//         this.cartsByLocation = new Map<LocationId, CartSchedule[]>();
+    private readonly allCarts: Cart[];
+    private readonly cartsByLocation: Map<LocationId, OutOfServiceJob[]>;
+    private readonly cartCapacity = 10;
 
-//         this.generateAllShifts(crews);
+    constructor(inService: Interval, crews: Crew[]) {
+        this.inService = inService;
+        this.clock = new Clock();
+        this.cartFactory = new CartFactory();
+        this.jobFactory = new JobFactory();
+        this.pauses = [];
 
-//         // Allocate and assign carts for each worker on each Shift.
-//         this.clock.mainloop();
+        this.allCarts = [];
+        this.cartsByLocation = new Map<LocationId, OutOfServiceJob[]>();
 
-//         // TODO: the factory should add jobs to the list.
-//         // Also, can compute inservice period from staffing plans associated with crews
-//         compileError();
-//         // for (const [location, carts] of this.cartsByLocation.entries()) {
-//         //     for (const cart of carts) {
-//         //         for (const job of cart.jobs) {
-//         //             this.pauses.push(job);
-//         //         }
-//         //     }
-//         // }
-//     }
+        this.generateAllShifts(crews);
 
-//     private createJob(suspendLocation: LocationId, suspendTime: SimTime, resumeTime: SimTime): OutOfServiceJob {
-//         const job = this.jobFactory.outOfService(suspendLocation, suspendTime, resumeTime);
-//         this.pauses.push(job);
-//         return job;
-//     }
+        // Allocate and assign carts for each worker on each Shift.
+        this.clock.mainloop();
 
-//     // Generator of Carts to be added to the environment.
-//     *carts() {
-//         for (const [location, fleet] of this.cartsByLocation) {
-//             for (const cart of fleet) {
-//                 yield cart;
-//             }
-//         }
-//     }
+        // TODO: the factory should add jobs to the list.
+        // Also, can compute inservice period from staffing plans associated with crews
+        // compileError();
+        // for (const [location, carts] of this.cartsByLocation.entries()) {
+        //     for (const cart of carts) {
+        //         for (const job of cart.jobs) {
+        //             this.pauses.push(job);
+        //         }
+        //     }
+        // }
+    }
 
-//     // Generator of OutOfServiceJobs corresponding to breaks.
-//     jobs() {
-//         return this.pauses[Symbol.iterator]();
-//     }
+    private createJob(suspendLocation: LocationId, suspendTime: SimTime, resumeTime: SimTime): OutOfServiceJob {
+        const job = this.jobFactory.outOfService(suspendLocation, suspendTime, resumeTime);
+        this.pauses.push(job);
+        return job;
+    }
 
-//     private generateAllShifts(crews: Crew[]) {
-//         for (const crew of crews) {
-//             for (let i = 0; i < crew.size; ++i) {
-//                 start(this.generateOneShift(crew.shift));
-//             }
-//         }
-//     }
+    // Generator of Carts to be added to the environment.
+    *carts(): IterableIterator<Cart> {
+        for (const [location, fleet] of this.cartsByLocation) {
+            for (const cart of fleet) {
+                // We know that jobs in this.cartsByLocation are always assigned.
+                // (never null)
+                yield cart.assignedTo as Cart;
+            }
+        }
+    }
 
-//     private *generateOneShift(shift: Shift) {
-//         yield this.clock.until(shift.working.start);
-//         const cart: CartSchedule = this.allocateCart(shift.home);
+    // Generator of OutOfServiceJobs corresponding to breaks.
+    jobs() {
+        return this.pauses[Symbol.iterator]();
+    }
 
-//         for (const b of shift.breaks) {
-//             cart.outOfService(b.interval, b.location);
-//             // this.pauses.push(job);
-//         }
+    private generateAllShifts(crews: Crew[]) {
+        for (const crew of crews) {
+            for (let i = 0; i < crew.size; ++i) {
+                start(this.generateOneShift(crew.shift));
+            }
+        }
+    }
 
-//         yield this.clock.until(shift.working.end);
-//         this.returnCart(cart);
-//     }
+    private *generateOneShift(shift: Shift) {
+        yield this.clock.until(shift.working.start);
 
-//     private allocateCart(location: LocationId): CartSchedule {
-//         const carts = this.cartsByLocation.get(location);
-//         if (carts !== undefined) {
-//             const cart = carts.pop();
-//             if (cart) {
-//                 return cart;
-//             }
-//             else {
-//                 return new CartSchedule(this.cartFactory, this.jobFactory, this.cartCapacity, location, this.inService);
-//                 // return this.cartFactory.cart(this.cartCapacity, location);
-//             }
-//         }
-//         else {
-//             this.cartsByLocation.set(location, []);
-//             return new CartSchedule(this.cartFactory, this.jobFactory, this.cartCapacity, location, this.inService);
-//             // return this.cartFactory.cart(this.cartCapacity, location);
-//         }
-//     }
+        // AllocateCart returns the final OutOfServiceJob for a cart.
+        // Update its resume time from MAX_DATE to the start of the shift.
+        const endOfLastShift = this.allocateCart(shift.home);
+        if (endOfLastShift.resumeTime !== MAX_DATE) {
+            // const message = 'Next Shift must start after previous Shift ends.';
+            // THIS IS A BUG
+            const message = "Expected job to resume at MAX_DATE";
+            throw TypeError(message);
+        }
+        endOfLastShift.resumeTime = shift.working.start;
 
-//     private createCart(location: LocationId) {
-//         const cart = this.cartFactory.cart(this.cartCapacity, location);
-//         compileError();
-//     }
+        if (!endOfLastShift.assignedTo) {
+            const message = "Expected cart assignment"
+            throw TypeError(message);
+        }
+        const cart = endOfLastShift.assignedTo;
 
-//     private returnCart(cart: CartSchedule) {
-//         const carts = this.cartsByLocation.get(cart.cart.lastKnownLocation) as CartSchedule[];
-//         carts.push(cart);
-//     }
-// }
+        // Create a new OutOfService job for the end of the shift.
+        const endOfCurrentShift = this.jobFactory.outOfService(shift.home, shift.working.end, MAX_DATE);
+        endOfCurrentShift.assignedTo = cart;
+        this.pauses.push(endOfCurrentShift);
 
-// ///////////////////////////////////////////////////////////////////////////////
-// //
-// // Utility functions relating to Shift construction.
-// //
-// ///////////////////////////////////////////////////////////////////////////////
+        // Add and OutOfServiceJob for each break.
+        for (const b of shift.breaks) {
+            if (!contains(shift.working, b.interval)) {
+                const message = 'Out of service interval must be contained by in service interval.';
+                throw TypeError(message);
+            }
+    
+            const breakPeriod = this.jobFactory.outOfService(b.location, b.interval.start, b.interval.end);
+            breakPeriod.assignedTo = cart;
+            this.pauses.push(breakPeriod);
+        }
 
-// // Constructs a standard 8 hour shift with the following out-of-service periods:
-// //   15 minute morning break after 2 hours - in break room
-// //   30 minute lunch break after 4 hours - in break room
-// //   15 minute afternoon break after 6 hours - in break room
-// //    5 minute return to base after 7:55 - at home location
-// export function standardShift(start: SimTime, home: LocationId, breakRoom: LocationId): Shift {
-//     return {
-//         working: interval(start, 0, 8 * HOUR),
-//         breaks: [
-//             // Morning break
-//             { interval: interval(start, 120 * MINUTE, 15 * MINUTE), location: breakRoom },
+        yield this.clock.until(shift.working.end);
+        this.returnCart(endOfCurrentShift);
+    }
 
-//             // Lunch
-//             { interval: interval(start, 240 * MINUTE, 30 * MINUTE), location: breakRoom },
+    private allocateCart(location: LocationId): OutOfServiceJob {
+        const carts = this.cartsByLocation.get(location);
+        if (carts !== undefined) {
+            const cart = carts.pop();
+            if (cart) {
+                return cart;
+            }
+            else {
+                return this.createCart(location);
+            }
+        }
+        else {
+            this.cartsByLocation.set(location, []);
+            return this.createCart(location);
+        }
+    }
 
-//             // Afternoon break
-//             { interval: interval(start, 360 * MINUTE, 15 * MINUTE), location: breakRoom },
+    private createCart(home: LocationId): OutOfServiceJob {
+        const cart = this.cartFactory.cart(this.cartCapacity, home);
+        this.allCarts.push(cart);
 
-//             // End of shift
-//             // TODO: consider alternatives to the following hack.
-//             // NOTE: End shift one unit of time early so that cart return will
-//             // be processed before next shift allocates a cart.
-//             { interval: interval(start, 475 * MINUTE, 5 * MINUTE - 1), location: home }
-//         ],
-//         home
-//     }
-// }
+        const outOfService = this.jobFactory.outOfService(home, MIN_DATE, MAX_DATE);
+        outOfService.assignedTo = cart;
+        this.pauses.push(outOfService);
+        // const before = this.jobFactory.outOfService(home, MIN_DATE, this.inService.start);
+        // before.assignedTo = cart;
+        // this.pauses.push(before);
+        // const after = this.jobFactory.outOfService(home, this.inService.end, MAX_DATE);
+        // after.assignedTo = cart;
+        // this.pauses.push(after);
 
-// // Constructs an Interval.
-// function interval(base: SimTime, offset: SimTime, length: SimTime): Interval {
-//     return {
-//         start: base + offset,
-//         end: base + offset + length
-//     };
-// }
+        return outOfService;
+    }
 
-// // Moves an Interval in time.
-// function adjustInterval(interval: Interval, offset: number): Interval {
-//     return {
-//         start: interval.start + offset,
-//         end: interval.end + offset
-//     };
-// }
+    private returnCart(job: OutOfServiceJob) {
+        const cart = job.assignedTo as Cart;
+        const jobs = this.cartsByLocation.get(cart.lastKnownLocation) as OutOfServiceJob[];
+        jobs.push(job);
+    }
+}
 
-// // Moves a Break in time.
-// function adjustBreak(b: Break, offset: SimTime): Break {
-//     return {
-//         interval: adjustInterval(b.interval, offset),
-//         location: b.location
-//     };
-// }
+///////////////////////////////////////////////////////////////////////////////
+//
+// Utility functions relating to Shift construction.
+//
+///////////////////////////////////////////////////////////////////////////////
 
-// // Moves a Shift in time.
-// export function adjustShift(shift: Shift, offset: number): Shift {
-//     return {
-//         working: adjustInterval(shift.working, offset),
-//         breaks: shift.breaks.map((x) => adjustBreak(x, offset)),
-//         home: shift.home
-//     };
-// }
+// Constructs a standard 8 hour shift with the following out-of-service periods:
+//   15 minute morning break after 2 hours - in break room
+//   30 minute lunch break after 4 hours - in break room
+//   15 minute afternoon break after 6 hours - in break room
+//    5 minute return to base after 7:55 - at home location
+export function standardShift(start: SimTime, home: LocationId, breakRoom: LocationId): Shift {
+    return {
+        working: interval(start, 0, 8 * HOUR - 1),
+        breaks: [
+            // Morning break
+            { interval: interval(start, 120 * MINUTE, 15 * MINUTE), location: breakRoom },
+
+            // Lunch
+            { interval: interval(start, 240 * MINUTE, 30 * MINUTE), location: breakRoom },
+
+            // Afternoon break
+            { interval: interval(start, 360 * MINUTE, 15 * MINUTE), location: breakRoom }
+
+            // // End of shift
+            // // TODO: consider alternatives to the following hack.
+            // // NOTE: End shift one unit of time early so that cart return will
+            // // be processed before next shift allocates a cart.
+            // { interval: interval(start, 475 * MINUTE, 5 * MINUTE - 2), location: home }
+        ],
+        home
+    }
+}
+
+// Constructs an Interval.
+function interval(base: SimTime, offset: SimTime, length: SimTime): Interval {
+    return {
+        start: base + offset,
+        end: base + offset + length
+    };
+}
+
+// Moves an Interval in time.
+function adjustInterval(interval: Interval, offset: number): Interval {
+    return {
+        start: interval.start + offset,
+        end: interval.end + offset
+    };
+}
+
+// Moves a Break in time.
+function adjustBreak(b: Break, offset: SimTime): Break {
+    return {
+        interval: adjustInterval(b.interval, offset),
+        location: b.location
+    };
+}
+
+// Moves a Shift in time.
+export function adjustShift(shift: Shift, offset: number): Shift {
+    return {
+        working: adjustInterval(shift.working, offset),
+        breaks: shift.breaks.map((x) => adjustBreak(x, offset)),
+        home: shift.home
+    };
+}
 
