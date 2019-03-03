48f861e08be5fcfe1199f5ecb9d0fdcee8979531
Mike Hopcroft <mhop@microsoft.com>
2019-03-03 18:52:06 -0500

    Renamed plan.ts to route.ts

--- src/planner/index.ts
+++ src/planner/index.ts
@@ -1,7 +1,7 @@
 export * from './combinations';
 export * from './job-assigner';
 export * from './job-merger';
-export * from './plan';
+export * from './route';
 export * from './route-planner';
 export * from './trie';
 
